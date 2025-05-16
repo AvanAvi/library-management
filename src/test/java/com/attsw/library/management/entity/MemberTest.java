53e760747e2a15ade03fe9b5b2fa97e53064bd61
AvanAvi <theastrofreek@gmail.com>
2025-05-16 20:39:25 +0200

    RED: Add failing test for Member JPA annotations

--- src/test/java/com/attsw/library/management/entity/MemberTest.java
+++ src/test/java/com/attsw/library/management/entity/MemberTest.java
@@ -14,11 +14,21 @@ class MemberTest {
 
     @Test
     void testMemberWithNameAndEmail() {
-        // RED 
+        // GREEN 
         Member member = new Member(1L, "Avan Avi", "avan.avi@email.com");
         
         assertEquals(1L, member.getId());
         assertEquals("Avan Avi", member.getName());
         assertEquals("avan.avi@email.com", member.getEmail());
+    }
+
+    @Test
+    void testMemberIsJpaEntity() {
+        // RED 
+        Member member = new Member(1L, "Avan Avi", "avan.avi@email.com");
+        
+      
+        assertTrue(member.getClass().isAnnotationPresent(jakarta.persistence.Entity.class));
+        assertNotNull(member.getId());
     }
 }
