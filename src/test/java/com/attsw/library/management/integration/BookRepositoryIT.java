6089c8bd49bdbd81a9e77fd4f4abdcf49f26f31a
AvanAvi <theastrofreek@gmail.com>
2025-05-16 23:29:45 +0200

    RED: Add failing BookRepository integration test

---
+++ src/test/java/com/attsw/library/management/integration/BookRepositoryIT.java
@@ -0,0 +1,29 @@
+package com.attsw.library.management.integration;
+
+import com.attsw.library.management.entity.Book;
+import com.attsw.library.management.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class BookRepositoryIT {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    void testSaveBook() {
+        // RED - This test will fail because BookRepository doesn't exist yet
+        Book book = new Book(null, "Clean Code", "Robert Martin", "123456789", 2008, "Programming");
+        
+        Book savedBook = bookRepository.save(book);
+        
+        assertNotNull(savedBook.getId());
+        assertEquals("Clean Code", savedBook.getTitle());
+    }
+}
