2b670e0cbed9d857e5c592fcdca3b09958b09249
AvanAvi <theastrofreek@gmail.com>
2025-05-28 17:55:15 +0200

    GREEN: Implement MemberService with saveMember method

---
+++ src/main/java/com/attsw/library/management/service/MemberService.java
@@ -0,0 +1,21 @@
+package com.attsw.library.management.service;
+
+import com.attsw.library.management.entity.Member;
+import com.attsw.library.management.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Member saveMember(Member member) {
+        return memberRepository.save(member);
+    }
+}
